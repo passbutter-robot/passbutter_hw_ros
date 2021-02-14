0da2ba33788f9425d4d2c795852b4f13e6d72704
Manuel Meyer <manuel.meyer.extern@googlemail.com>
2021-02-14 09:52:26 +0100

    add launch file for axis control. add base configuration.

---
+++ passbutter_driver/launch/axis_control.launch.py
@@ -0,0 +1,51 @@
+import launch
+import launch_ros.actions
+import os
+import pathlib
+import yaml
+
+from ament_index_python.packages import get_package_share_directory
+
+parameters_file_name = 'default.yaml'
+
+def generate_launch_description():
+    config_dir = os.path.join(get_package_share_directory('passbutter_driver'), 'config')
+    param_config = os.path.join(config_dir, "axis_control.yaml")
+    with open(param_config, 'r') as f:
+        axis_0_params = yaml.safe_load(f)["axis_0"]["ros__parameters"]
+
+    with open(param_config, 'r') as f:
+        axis_1_params = yaml.safe_load(f)["axis_1"]["ros__parameters"]
+
+    with open(param_config, 'r') as f:
+        axis_2_params = yaml.safe_load(f)["axis_2"]["ros__parameters"]
+
+    with open(param_config, 'r') as f:
+        axis_3_params = yaml.safe_load(f)["axis_3"]["ros__parameters"]
+
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='passbutter_driver', executable='single_stepper_driver', output='screen',
+            name='axis_0',
+            parameters=[
+                axis_0_params
+            ]),
+        launch_ros.actions.Node(
+            package='passbutter_driver', executable='single_stepper_driver', output='screen',
+            name='axis_1',
+            parameters=[
+                axis_1_params
+            ]),
+        launch_ros.actions.Node(
+            package='passbutter_driver', executable='single_stepper_driver', output='screen',
+            name='axis_2',
+            parameters=[
+                axis_2_params
+            ]),
+        launch_ros.actions.Node(
+            package='passbutter_driver', executable='single_stepper_driver', output='screen',
+            name='axis_3',
+            parameters=[
+                axis_3_params
+            ])
+    ])
--- passbutter_driver/src/single_stepper_driver_node.cpp
+++ passbutter_driver/src/single_stepper_driver_node.cpp
@@ -9,14 +9,49 @@ SingleStepperDriverNode::SingleStepperDriverNode()
 {
     RCLCPP_INFO(this->get_logger(), "initialize node 'single_stepper_driver'");
 
+    rcl_interfaces::msg::ParameterDescriptor bus_number_descriptor;
+    bus_number_descriptor.name = "bus_number";
+    bus_number_descriptor.type = rcl_interfaces::msg::ParameterType::PARAMETER_INTEGER;
+    bus_number_descriptor.description = "the thunderborg i2c bus number";
+    this->declare_parameter("bus_number", 0, bus_number_descriptor);
+
+    rcl_interfaces::msg::ParameterDescriptor board_address_descriptor;
+    board_address_descriptor.name = "board_address";
+    board_address_descriptor.type = rcl_interfaces::msg::ParameterType::PARAMETER_INTEGER;
+    board_address_descriptor.description = "the thunderborg i2c board address";
+    this->declare_parameter("board_address", 0, board_address_descriptor);
+
+    int busNumber = -1;
+    this->get_parameter<int>("bus_number", busNumber);
+
+    int boardAddress = -1;
+    this->get_parameter<int>("board_address", boardAddress);
+
+    RCLCPP_INFO(this->get_logger(), "bus number=%d", busNumber);
+    RCLCPP_INFO(this->get_logger(), "board address=%d", boardAddress);
+
     this->_steps = this->create_subscription<example_interfaces::msg::Int32>("steps", 10,
             std::bind(&SingleStepperDriverNode::stepperCallback, this, std::placeholders::_1));
 
     this->stepperControl = new passbutter::StepperControl("stepper", 0.3, 0.1);
-    std::vector<int> boardAddrs = this->stepperControl->detectBoards(1);
+
+    std::vector<int> boardAddrs;
+    if (busNumber < 0 || boardAddress < 0)
+    {
+        busNumber = 1;
+        RCLCPP_INFO(this->get_logger(), "try to autodetect board address on bus=%d", busNumber);
+        boardAddrs = this->stepperControl->detectBoards(busNumber);
+    }
+    else
+    {
+        RCLCPP_INFO(this->get_logger(), "try detect board address on bus=%d for address=%d", busNumber, boardAddress);
+        boardAddrs = this->stepperControl->detectBoards(busNumber, boardAddress, boardAddress + 1);
+    }
+
     if (boardAddrs.size() == 0)
     {
         RCLCPP_ERROR(this->get_logger(), "no boards detected..");
+        boardAddress = boardAddrs[0];
         this->foundBoard = false;
     }
     else
